6c4b402fc6c3cc8aa804f3f3ae95b4396ad36f5e
Pheonix1013 <pl1017970@ahschool.com>
2025-09-25 18:53:56 -0400

    Update api.py

--- api.py
+++ api.py
@@ -1,5 +1,7 @@
+#import flask and jsonify function from library
 from flask import Flask, jsonify
 
+#create API
 app = Flask(__name__)
 
 # robot controls state
@@ -10,15 +12,43 @@ controls = {
     "right": False
 }
 
+#define the branch of the api and the method "post"
 @app.route("/<direction>", methods=["POST"])
+
+#create move function
 def move(direction):
+    """
+    Updates the direction in the dictionary
+    If value for the specific direction is True, it well set to False and vice versa.
+    
+    Parameters:
+    Direction
+    
+    Return:
+    Jsonified dictionary with updated controls
+    """
+    
     if direction in controls:
         controls[direction] = not controls[direction]
         return jsonify({direction: controls[direction]})
 
+#define the branch of the api and the method "get"
 @app.route("/status", methods=["GET"])
+
+#create the status function
 def status():
+    """
+    Displays the current state of the controls
+    
+    Parameters:
+    None
+    
+    Return:
+    Jsonified list of current controls
+	"""
+    
     return jsonify(controls)
+
 
 if __name__ == "__main__":
     app.run(port=5000)
